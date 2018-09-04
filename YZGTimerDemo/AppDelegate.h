ec2c21ff752cd78573b8c6a5cb0b4e39a2848129
yanzhigang <yanzhigang@hualala.com>
2018-09-04 19:29:12 +0800

    Initial Commit

---
+++ YZGTimerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YZGTimerDemo
+//
+//  Created by HLL on 2018/9/4.
+//  Copyright © 2018年 阎志刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YZGTimerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YZGTimerDemo
+//
+//  Created by HLL on 2018/9/4.
+//  Copyright © 2018年 阎志刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
